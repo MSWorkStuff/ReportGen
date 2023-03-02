3946824abeecea427d4879b631acf8f102d9c644
José Álvarez <josealvar@microsoft.com>
2023-03-02 15:07:27 -0800

    wip

--- services/ReportService.cs
+++ services/ReportService.cs
@@ -1,28 +1,37 @@
 using System.Text.Json;
-
+using System.IO;
 namespace Services;
 
 public class ReportService {
 
     public void Report<Input, Output>(
-        List<Entry<Input, Output>> entries,
-        Func<Input, Output> generator) {
+            List<Entry<Input, Output>> entries,
+            Func<Input, Output> generator) {
+        using var fileWriter = GetFileHandle();
 
         foreach(var entry in entries) {
+            fileWriter.WriteLine($"## {entry.title}");
+            fileWriter.WriteLine();
+
+            fileWriter.WriteLine($"### Input JSON");
             var inputJson = JsonSerializer.Serialize(entry.input, new JsonSerializerOptions { WriteIndented = true });
+            fileWriter.WriteLine(inputJson);
+            fileWriter.WriteLine();
 
+            fileWriter.WriteLine($"### Output JSON");
             var output = generator.Invoke(entry.input);
-
             var outputJson = JsonSerializer.Serialize(output, new JsonSerializerOptions { WriteIndented = true });
-
-            Console.WriteLine(inputJson);
-            Console.WriteLine(outputJson);
+            fileWriter.WriteLine(outputJson);
+            fileWriter.WriteLine();
         }
+    }
+
+    private StreamWriter GetFileHandle() {
+        var logFile = File.Create("report.md");
+        return new System.IO.StreamWriter(logFile);
     }
 }
 
-// CompletionsOptions == Input
-// Completions == Output
 public class Entry<Input, Output> {
     public string title { get; init;}
     public Input input { get; init; }
