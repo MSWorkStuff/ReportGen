ee887ef902d8c44b4ce6e1caea8edbb2cb343c6e
Jose Alvarez <josealvar@microsoft.com>
2023-03-02 10:26:02 -0800

    Made main async

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ReportGenerator;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
+
